157a3eceab71e97eec9ad32c34e56baa3b601e41
Ben Kiboma <omayioben@gmail.com>
2022-07-14 01:24:19 +0300

    Use Slice

---
+++ src/redux/bookSlice.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/prefer-default-export */
+import { createSlice } from '@reduxjs/toolkit';
+
+const bookSlice = createSlice({
+  name: 'books',
+  initialState: [
+    {
+      title: 'Crime',
+      author: 'John',
+      id: 1,
+    },
+    {
+      title: 'Crime2',
+      author: 'Kiriamiti',
+      id: 2,
+    },
+  ],
+  reducers: {
+    addBook: (state, action) => {
+      const newBook = {
+        id: Date.now(),
+        title: action.payload.title,
+        author: action.payload.author,
+      };
+      state.push(newBook);
+    },
+  },
+});
+
+export const { newBook } = bookSlice.actions;

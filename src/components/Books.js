e4dfeef0b037074e5c02f48af74e6c551f8d8ccf
Ben Kiboma <omayioben@gmail.com>
2022-07-16 00:41:27 +0300

    Style pregressive bar

--- src/components/Books.js
+++ src/components/Books.js
@@ -22,28 +22,31 @@ const Books = (props) => {
     <div className="display-book">
       <div className="book-details">
         <div className="books-uppersection">
-          <p>{booktypes[Math.floor(Math.random() * booktypes.length)]}</p>
-          <h2>{title}</h2>
-          <p>{author}</p>
+          <p className="book-type">{booktypes[Math.floor(Math.random() * booktypes.length)]}</p>
+          <h2 className="book-title">{title}</h2>
+          <p className="book-author">{author}</p>
         </div>
         <div className="books-lowersection">
           <button type="button" className="bookdisplay-btn">Comments</button>
-          <button onClick={() => handleRemove(id)} type="button">Remove</button>
+          <div className="line" />
+          <button onClick={() => handleRemove(id)} type="button" className="bookdisplay-btn">Remove</button>
+          <div className="line" />
           <button type="button" className="bookdisplay-btn">Edit</button>
         </div>
       </div>
       <div className="progress-bar">
-        <div className="progress-chart" style={{ width: 50, height: 50 }}>
+        <div className="progress-chart" style={{ width: 80, height: 80 }}>
           <CircularProgressbar value={randomperc1} />
         </div>
         <div>
-          <p>
+          <p className="progress-percentage">
             {randomperc1}
             %
           </p>
-          <p>Completed</p>
+          <p className="progress-completed">Completed</p>
         </div>
       </div>
+      <div className="line2" />
       <div className="progress-chapter">
         <p>CURRENT CHAPTER</p>
         <p>

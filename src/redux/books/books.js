77af3114fbea56d7619282de55551afa660c9449
Ben Kiboma <omayioben@gmail.com>
2022-07-15 13:52:20 +0300

    Generate API Add GET book reducer Add Create book

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,33 +1,17 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const GET = 'bookstore/books/GET';
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
-const defaultState = [
-  {
-    title: 'Crime',
-    author: 'John',
-    id: 1,
-  },
-  {
-    title: 'Crime2',
-    author: 'Kiriamiti',
-    id: 2,
-  },
-];
+const baseURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/OkbgJL1hxwuld1j7wBke/books';
 
-export const addBook = (id) => ({
-  type: ADD_BOOK,
-  id,
-});
-
-export const removeBook = (id) => ({
-  type: REMOVE_BOOK,
-  id,
-});
-
-const booksReducer = (state = defaultState, action) => {
+const booksReducer = (state = [], action) => {
   switch (action.type) {
+    case GET:
+      return action.book;
     case ADD_BOOK:
       return [...state, action.id];
-
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.id);
     default:
@@ -35,4 +19,43 @@ const booksReducer = (state = defaultState, action) => {
   }
 };
 
+export const addBook = (book) => ({
+  type: ADD_BOOK,
+  book,
+});
+
+export const removeBook = (id) => ({
+  type: REMOVE_BOOK,
+  id,
+});
+
+export const getBook = (book) => ({
+  type: GET,
+  book,
+});
+
+export const addNewBook = (book) => async (dispatch) => {
+  const {
+    title, author, id, category,
+  } = book;
+  const newBook = {
+    item_id: id,
+    title,
+    author,
+    category,
+  };
+  // post the new book in the server
+  await axios.post(baseURL, newBook);
+  dispatch(addBook(book));
+};
+
+export const getBooksToDisplay = createAsyncThunk(GET, async () => {
+  const books = await axios.get(baseURL);
+  const objectOfBooks = Object.entries(books.data).map(([id, book]) => {
+    const { title, author } = book[0];
+    return { id, title, author };
+  });
+
+  return objectOfBooks;
+});
 export default booksReducer;

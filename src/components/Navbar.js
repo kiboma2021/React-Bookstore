9ff9f74cff25ce31d25f263b86fc4749b3ad33da
Ben Kiboma <omayioben@gmail.com>
2022-07-16 01:27:51 +0300

    Style navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+// import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Navbar = () => {
   const { books } = useSelector((state) => state.books);
   return (
     <nav>
-      <div>
-        <h1>Bookstore CMS</h1>
+      <div className="left-nav">
+        <div>
+          <h1>Bookstore CMS</h1>
+        </div>
+        <ul>
+          <li>
+            <NavLink to="/">
+              BOOKS(
+              {books.length}
+              )
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/categories">CATEGORIES</NavLink>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>
-          <NavLink to="/">
-            BOOKS(
-            {books.length}
-            )
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/categories">CATEGORIES</NavLink>
-        </li>
-      </ul>
+
     </nav>
   );
 };

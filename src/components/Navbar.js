604dabf6203f22f9ca107bee44ab2d30c8c59414
Ben Kiboma <omayioben@gmail.com>
2022-07-16 11:52:49 +0300

    Install user profile icon Style logo and header section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-// import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import '../Styles/Navbar.css';
 
 const Navbar = () => {
   const { books } = useSelector((state) => state.books);
   return (
     <nav>
       <div className="left-nav">
-        <div>
+        <div className="logo">
           <h1>Bookstore CMS</h1>
         </div>
         <ul>
@@ -24,7 +25,11 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
-
+      <div className="right-nav">
+        <div className="avatar">
+          <AccountCircleIcon fontSize="large" sx={{ color: '#0290ff' }} />
+        </div>
+      </div>
     </nav>
   );
 };
